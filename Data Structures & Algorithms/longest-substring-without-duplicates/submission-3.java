16eae39178957ff5d1e22b8dbb29ecfd9666e77b
Glyph <88108102+AnkitGang@users.noreply.github.com>
2026-05-23 00:17:39 +0530

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // Sliding Window
+        int left = 0;
+        int right = 0;
+        HashSet<Character> set = new HashSet<>();
+        int ans = 0;
+
+        while(right < s.length()) {
+            if(!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                ans = Math.max(ans, set.size());
+                right++;
+            }
+            else {
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+
+        return ans;
+    }
+}
