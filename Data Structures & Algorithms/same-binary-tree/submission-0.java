c6a837daca2ae4c542080effa72ca1420af2fe4e
Glyph <88108102+AnkitGang@users.noreply.github.com>
2026-06-15 02:25:20 +0530

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    boolean status = true;
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null)
+            return true;
+
+        if (!status)
+            return false;
+        
+        if(p != null && q != null && p.val == q.val) {
+            isSameTree(p.left, q.left);
+            isSameTree(p.right, q.right);
+        }
+        else {
+            status = false;
+        }
+
+        return status;
+    }
+}
