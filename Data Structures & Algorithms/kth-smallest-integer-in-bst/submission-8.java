1b246e7094266b8c3a95ffd4f0a0633eb1b74730
Glyph <88108102+AnkitGang@users.noreply.github.com>
2026-06-27 00:57:59 +0530

    Add: kth-smallest-integer-in-bst - submission-8

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-8.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int counter = 0;
+    int res = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        find(root, k);
+        return res;
+    }
+
+    private void find(TreeNode root, int k) {
+         if(root == null)
+            return;
+
+        find(root.left, k);
+        counter++;
+        if(counter == k) {
+            res = root.val;
+            return;
+        }
+
+        find(root.right, k);
+    }
+}
